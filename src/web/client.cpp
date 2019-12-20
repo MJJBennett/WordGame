a9c3cc43030eabf2c15e36d92e0e580557cc425d
Michael Bennett <michaelbennett.dev@gmail.com>
2019-12-19 22:35:37 -0800

    Improved comments and logging, added very rudimentary message queuing.

--- src/web/client.cpp
+++ src/web/client.cpp
@@ -4,17 +4,22 @@
 
 wg::WebSocketClient::WebSocketClient() : resolver_(ioc_), ws_(ioc_)
 {
-    wg::log::point("Constructing websocket wrapper.");
+    wg::log::point("[Client] Constructing websocket wrapper.");
     boost::beast::error_code ec{};
 
 #include "VERYTEMP_remoteaddr.tmp"
+    message_queue_.push("First message!");
+    message_queue_.push("Hello again!");
+    message_queue_.push("Hello my friend!");
+    message_queue_.push("Okay, last time's the charm!");
 
-    wg::log::point("Connecting to websocket at URL: ", remote_address, " and port: ", 27600);
+    wg::log::point("[Client] Connecting to websocket at URL: ", remote_address,
+                   " and port: ", 27600);
 
-    wg::log::point("Resolving websocket URL.");
+    wg::log::point("[Client] Resolving websocket URL.");
     auto const results = resolver_.resolve(remote_address, "27600");
 
-    wg::log::point("Connecting to the IP address using Asio.");
+    wg::log::point("[Client] Connecting to the IP address using Asio.");
     beast::get_lowest_layer(ws_).connect(results, ec);
     if (ec)
     {
@@ -33,37 +38,57 @@ wg::WebSocketClient::WebSocketClient() : resolver_(ioc_), ws_(ioc_)
                 std::string(BOOST_BEAST_VERSION_STRING) + " websocket-wordgame-client");
     }));
 
-    wg::log::point("Performing the websocket handshake.");
+    wg::log::point("[Client] Performing the websocket handshake.");
     ws_.handshake(remote_address, std::string("/"));
-    wg::log::point("Successfully performed the handshake! Waiting for launch() call.");
+    wg::log::point("[Client] Successfully performed the handshake! Waiting for launch() call.");
 }
 
 void wg::WebSocketClient::on_write(beast::error_code ec, std::size_t)
 {
     if (ec)
     {
-        wg::log::err("[WebSocketClient] Write failed: ", ec.message());
+        wg::log::err("[Client] Write failed: ", ec.message());
         return;
     }
 
-    // done for now, just testing
+    message_.reset();
+
+    // Simple queueing system for setup and testing
+    if (message_queue_.size())
+    {
+        message_ = message_queue_.front();
+        message_queue_.pop();
+        ws_.async_write(asio::buffer(*message_),
+                        beast::bind_front_handler(&WebSocketClient::on_write, shared_from_this()));
+    }
+    else
+    {
+        shutdown();
+    }
 }
 
 void wg::WebSocketClient::launch()
 {
     // note: cannot call shared_from_this in the constructor
     // today, I have learned something new! (heh, memory pun)
-    ws_.async_write(asio::buffer(persistent_str_),
-        beast::bind_front_handler(&WebSocketClient::on_write, shared_from_this()));
+    ws_.async_write(asio::buffer(*message_),
+                    beast::bind_front_handler(&WebSocketClient::on_write, shared_from_this()));
 
     ioc_.run();
 }
 
 void wg::WebSocketClient::shutdown()
 {
+    wg::log::point("[Client] Shutting down web client.");
+    if (status_ == Status::Closed)
+    {
+        wg::log::warn("[Client] Web socket was already closed.");
+        return;
+    }
     try
     {
         ws_.close(websocket::close_code::normal);
+        status_ = Status::Closed;
     }
     catch (const std::exception& e)
     {
--- src/web/client.hpp
+++ src/web/client.hpp
@@ -5,6 +5,8 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/websocket.hpp>
 #include <memory>
+#include <optional>
+#include <queue>
 
 namespace wg
 {
@@ -19,21 +21,29 @@ class WebSocketClient : public std::enable_shared_from_this<WebSocketClient>
 public:
     WebSocketClient();
 
+    // Blocking call, launches the web socket
     void launch();
     void shutdown();
 
     void on_write(beast::error_code, std::size_t);
 
 private:
-    // This must be constructed first
+    // The client has its own io context
     boost::asio::io_context ioc_;
-
     boost::asio::ip::tcp::resolver resolver_;
 
-    // This uses the SSL context and the IOC
+    // The core of this class - a wrapped websocket stream
     websocket::stream<beast::tcp_stream> ws_;
 
-    std::string persistent_str_{"Hello from client!"};
+    std::queue<std::string> message_queue_;
+    std::optional<std::string> message_{"Zero string!"};
+
+    enum class Status
+    {
+        Ready,
+        Running,
+        Closed
+    } status_ = Status::Ready;
 };
 }  // namespace wg
 
