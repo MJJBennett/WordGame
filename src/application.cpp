76f50993bb56f7988c3c55ee75babb32622aad28
Michael Bennett <michaelbennett.dev@gmail.com>
2019-12-22 23:04:10 -0800

    try/catch to make it work, yay

--- src/application.cpp
+++ src/application.cpp
@@ -1,6 +1,7 @@
 #include "application.hpp"
 
 #include <SFML/Graphics.hpp>
+#include <nlohmann/json.hpp>
 #include <thread>
 #include "client.hpp"
 #include "debug/log.hpp"
@@ -13,7 +14,6 @@
 #include "game/context.hpp"
 #include "game/item.hpp"
 #include "server.hpp"
-#include <nlohmann/json.hpp>
 
 int wg::Application::launch(Mode mode)
 {
@@ -34,9 +34,12 @@ int wg::Application::launch(Mode mode)
     const auto choice = wg::window_io::get_string(window, manager, "Game Menu", opts);
     wg::log::point("Entering mode: ", choice);
 
-    if (choice == "Launch Server") run_webserver(window, manager, renderer);
-    else if (choice == "Connect to Server") run_webclient(window, manager, renderer);
-    else if (choice == "Play Singleplayer (Lonely Mode)") run_local(window, manager, renderer);
+    if (choice == "Launch Server")
+        run_webserver(window, manager, renderer);
+    else if (choice == "Connect to Server")
+        run_webclient(window, manager, renderer);
+    else if (choice == "Play Singleplayer (Lonely Mode)")
+        run_local(window, manager, renderer);
 
     return 0;
 }
@@ -87,12 +90,12 @@ int wg::Application::run_webclient(wg::WindowContext& window, wg::ResourceManage
         while (window.getTarget().pollEvent(e))
         {
             if (e.type == sf::Event::Closed) window.close();
-            //if (e.type == sf::Event::KeyReleased) web_client.send("KeyEvent occurred!");
+            // if (e.type == sf::Event::KeyReleased) web_client.send("KeyEvent occurred!");
             game.parse_input(e);
             if (game.last_update)
             {
                 const auto u = *game.last_update;
-                const auto j = nlohmann::json{{"col",u.col},{"row",u.row},{"char",u.c}};
+                const auto j = nlohmann::json{{"col", u.col}, {"row", u.row}, {"char", u.c}};
                 game.last_update.reset();
                 web_client.send(j.dump());
             }
@@ -109,14 +112,22 @@ int wg::Application::run_webclient(wg::WindowContext& window, wg::ResourceManage
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
         // check for messages on the web client...
         auto str = web_client.read_once();
-        if (str) {
+        if (str)
+        {
             wg::log::data("Client message found", *str);
             // now let's make a hacky thing
-            const auto d = nlohmann::json::parse(*str);
-            const auto col = d["col"]; 
-            const auto row = d["row"]; 
-            const auto c = std::string{d["char"]}[0]; 
-            game.set_tile(col, row, c);
+            try
+            {
+                const auto d   = nlohmann::json::parse(*str);
+                const auto col = d["col"];
+                const auto row = d["row"];
+                const auto c   = std::string{d["char"]}[0];
+                game.set_tile(col, row, c);
+            }
+            catch (nlohmann::json::parse_error e)
+            {
+                wg::log::warn("Got json parse error: ", e.what());
+            }
         }
     }
 
