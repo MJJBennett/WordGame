bcca669e10657783bcafc5f2ec0049e21fae05ca
Michael Bennett <michaelbennett.dev@gmail.com>
2019-12-25 01:58:27 -0800

    Automatically get more dependencies.

--- scripts/grab-deps.sh
+++ scripts/grab-deps.sh
@@ -6,5 +6,20 @@ script_dir=$(dirname "$0")
 # Move to that directory so we can reliably move to the correct build directory
 cd $script_dir/..
 
+mkdir include/deps
+mkdir resources/Roboto
+
+if [ ! -e "resources/Roboto/LICENSE.txt" ]; then
+curl https://raw.githubusercontent.com/google/fonts/master/apache/robotomono/LICENSE.txt -o resources/Roboto/LICENSE.txt
+fi
+if [ ! -e "resources/Roboto/RobotoMono-Bold.ttf" ]; then
+curl https://github.com/google/fonts/raw/master/apache/robotomono/RobotoMono-Bold.ttf -o resources/Roboto/RobotoMono-Bold.ttf
+fi
+if [ ! -e "resources/Roboto/Roboto-Black.ttf" ]; then
+curl https://github.com/google/fonts/raw/master/apache/roboto/Roboto-Black.ttf -o resources/Roboto/Roboto-Black.ttf
+fi
+
+cp -a resources/Roboto/ output/
+
 # Clone nlohmann/json into dependencies folder
-git clone https://github.com/nlohmann/json.git deps/nlohmann_json
+git clone https://github.com/nlohmann/json.git include/deps/nlohmann_json
